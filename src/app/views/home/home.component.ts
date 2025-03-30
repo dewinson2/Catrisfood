c66a5d4d8b21c1b1327b70d9fcf2eb30e28e6a71
dewinson1 <dewinson12@gmail.com>
2025-03-30 15:08:23 -0400

    v6

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -4,10 +4,13 @@ import { HeroComponent } from '../../components/hero/hero.component';
 import { InfoComponent } from '../../components/info/info.component';
 import { MenuComponent } from '../../components/menu/menu.component';
 import { EventosComponent } from '../../components/eventos/eventos.component';
+import { UbicacionComponent } from '../../components/ubicacion/ubicacion.component';
+import { ReservaComponent } from '../../components/reserva/reserva.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, HeroComponent, InfoComponent, MenuComponent,EventosComponent],
+  imports: [HeaderComponent, HeroComponent, InfoComponent, MenuComponent,EventosComponent, UbicacionComponent,ReservaComponent,FooterComponent],
   
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
