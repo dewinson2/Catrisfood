33ea3440635390b304aabf3ceac16d98f2b44e17
dewinson1 <dewinson12@gmail.com>
2025-04-29 11:49:52 -0400

    ff

--- src/app/views/menu/menu.component.ts
+++ src/app/views/menu/menu.component.ts
@@ -37,8 +37,8 @@ export class MenuComponent {
       items: [
         {
           id: 1,
-          title: 'MINICAHNGAS LA GUEREJA',
-          description: '3 DELICIOSAS TORTILLAS DE TRIGO RELLENAS DE POLLO, SALSA ROJA Y UNA CREMA AGRIA QUE TE HARÁ BAILAR DE FELICIDAD..',
+          title: 'Minichangas La Guereja',
+          description: '3 deliciosas tortillas de trigo rellenas de pollo, salsa roja y una crema agria que te hará bailar de felicidad.',
           price: 380,
           image: 'https://placehold.co/600x400/4CAF50/ffffff?text=Minichangas',
           rating: 5,
@@ -46,8 +46,8 @@ export class MenuComponent {
         },
         {
           id: 2,
-          title: 'TOTOPOS CHESPIRITO',
-          description: 'TOTOPOS CRUJIENTES CON UNA FIESTA DE QUESOPICO DE GALLO Y GUACAMOLE Y UNA SALSA ROJA QUE BAILA EN TU BOCA.',
+          title: 'Totopos Chespirito',
+          description: 'Totopos crujientes con una fiesta de queso, pico de gallo y guacamole, y una salsa roja que baila en tu boca.',
           price: 380,
           image: 'totopos.jpg',
           rating: 5,
@@ -55,8 +55,8 @@ export class MenuComponent {
         },
         {
           id: 3,
-          title: 'Quesadilla frita doña florinda',
-          description: '3 deliciosas tortillas de maíz frita, abranzando queso blanco, salsa roja y una nube de crema agria.',
+          title: 'Quesadilla frita Doña Florinda',
+          description: '3 deliciosas tortillas de maíz fritas, abrazando queso blanco, salsa roja y una nube de crema agria.',
           price: 380,
           image: 'quesallidas.jpg',
           rating: 5,
@@ -77,10 +77,9 @@ export class MenuComponent {
           rating: 5,
           reviews: 24
         },
-       
         {
           id: 7,
-          title: 'Tostadistos Res Pollo Mixto o al Pastor',
+          title: 'Tostaditos Res Pollo Mixto o al Pastor',
           description: 'Tu proteína favorita, 4 sabrosas tortillas de trigo o maíz, queso delicioso, pico de gallo y un guacamole cremoso, salsa roja.',
           price: 490,
           image: 'tostadita.jpg',
@@ -88,23 +87,19 @@ export class MenuComponent {
           reviews: 16
         },
         {
-
-
-
           id: 8,
           title: 'Tacos de Birria',
-          description: 'Guiso de carne osobusco, 4 raciones, tortillas de maíz, queso, cilantro cebolla, consomé del guiso, y salsa picante que hace bailar los sabores.',
+          description: 'Guiso de carne osobuco, 4 raciones, tortillas de maíz, queso, cilantro, cebolla, consomé del guiso, y salsa picante que hace bailar los sabores.',
           price: 680,
           image: 'birria.jpg',
           rating: 5,
           reviews: 20
         },
-        
         {
           id: 9,
-          title: 'Tacos los chiflados res pollo o mixto',
-          description: 'Tu proteína favorita, 3 deliciosas opciones tortilla de Maíz o trigo, pico de gallo guacamole salsa roja y crema agria.¡Fiesta de sabores!',
-          price: 480 ,
+          title: 'Tacos Los Chiflados Res Pollo o Mixto',
+          description: 'Tu proteína favorita, 3 deliciosas opciones tortilla de maíz o trigo, pico de gallo, guacamole, salsa roja y crema agria. ¡Fiesta de sabores!',
+          price: 480,
           image: 'chiflados.jpg',
           rating: 5,
           reviews: 10
@@ -117,8 +112,8 @@ export class MenuComponent {
       items: [
         {
           id: 8,
-          title: 'TOTOPOS RANCHO GRANDE (NACHOS)',
-          description: 'TOTOPOS CRUJIENTES CARGADOS CON CARNE MOLIDA, QUESO, CREMA DE QUESO, CREMA AGRIA, GUACAMOLE Y PICO DE GALLO..',
+          title: 'Totopos Rancho Grande (Nachos)',
+          description: 'Totopos crujientes cargados con carne molida, queso, crema de queso, crema agria, guacamole y pico de gallo.',
           price: 580,
           image: 'RanchoGrande.jpg',
           rating: 5,
@@ -126,8 +121,8 @@ export class MenuComponent {
         },
         {
           id: 9,
-          title: 'CHILAQUILES CANTINFLAS RES-POLLO MIXTO',
-          description: 'TOTOPOS CRUJIENTES BAÑADOS EN SALSA ROJA O VERDE SUAVE O PICANTE, PROTEÍNA DE ELECCION, CREMA AGRIA, GUACAMOLE Y PICO DE GALLO.',
+          title: 'Chilaquiles Cantinflas Res-Pollo Mixto',
+          description: 'Totopos crujientes bañados en salsa roja o verde suave o picante, proteína de elección, crema agria, guacamole y pico de gallo.',
           price: 650,
           image: '',
           rating: 5,
@@ -135,8 +130,8 @@ export class MenuComponent {
         },
         {
           id: 10,
-          title: 'MIX DE TOTOPOS DE RES-POLLO O MIXTO',
-          description: 'TOTOPOS CRUJIENTES, REMATADOS CON CARNE MECHADADE RES, POLLO, QUESO DERRETIDO, PICO DE GALLO,GUACAMOLE Y UNA NUBE DE CREMA AGRIA.',
+          title: 'Mix de Totopos de Res-Pollo o Mixto',
+          description: 'Totopos crujientes, rematados con carne mechada de res, pollo, queso derretido, pico de gallo, guacamole y una nube de crema agria.',
           price: 580,
           image: '',
           rating: 5,
@@ -145,13 +140,13 @@ export class MenuComponent {
       ]
     },
     {
-      id: 'QUESADILLAS',
-      name: 'QUESADILLAS',
+      id: 'quesadillas',
+      name: 'Quesadillas',
       items: [
         {
           id: 12,
-          title: 'QUESADILLAS CATRINA',
-          description: 'QUESADILLA CON TORTILLA DE TRIGO XL RELLENA DE QUESO.',
+          title: 'Quesadillas Catrina',
+          description: 'Quesadilla con tortilla de trigo XL rellena de queso.',
           price: 490,
           image: '',
           rating: 4,
@@ -159,8 +154,8 @@ export class MenuComponent {
         },
         {
           id: 13,
-          title: 'QUESADILLA A LA MULA',
-          description: 'TORTILLA DE TRIGO XL REPLETA DE QUESO, CARNE AL PASTOR, PICO DEGALLO, GUACAMOLE, SALSA ROJA Y CREMA AGRIA.',
+          title: 'Quesadilla a la Mula',
+          description: 'Tortilla de trigo XL repleta de queso, carne al pastor, pico de gallo, guacamole, salsa roja y crema agria.',
           price: 590,
           image: '',
           rating: 5,
@@ -169,13 +164,13 @@ export class MenuComponent {
       ]
     },
     {
-      id: 'BURRITOS',
-      name: 'BURRITOS',
+      id: 'burritos',
+      name: 'Burritos',
       items: [
         {
           id: 14,
-          title: 'BURRITOS BOJITAS RES-POLLO O MIXTO',
-          description: 'ELEGIR PROTEÍNA SABROSA, TORTILLA DE TRIGO XL CARGADA, LECHUGA,CARNE MECHADA, CREMA AGRIA, GUACAMOLE, PICO DE GALLO Y SALSA ROJA.',
+          title: 'Burritos Bojitas Res-Pollo o Mixto',
+          description: 'Elegir proteína sabrosa, tortilla de trigo XL cargada, lechuga, carne mechada, crema agria, guacamole, pico de gallo y salsa roja.',
           price: 490,
           image: 'bojitas.jpg',
           rating: 5,
@@ -183,8 +178,8 @@ export class MenuComponent {
         },
         {
           id: 15,
-          title: 'BURRITO DON BARRIGA MIXTO',
-          description: 'TORTILLA DE TRIGO XL CARGADA CON UN FESTÍN DE CARNE, TOCINETA, PLÁTANO MADURO, LECHUGA, SALSA ROJA, CREMA AGRIA Y AGUACATE.',
+          title: 'Burrito Don Barriga Mixto',
+          description: 'Tortilla de trigo XL cargada con un festín de carne, tocineta, plátano maduro, lechuga, salsa roja, crema agria y aguacate.',
           price: 630,
           image: 'barriga.jpg',
           rating: 5,
@@ -193,75 +188,70 @@ export class MenuComponent {
       ]
     },
     {
-      id: 'FAJITAS',
-      name: 'FAJITAS',
+      id: 'fajitas',
+      name: 'Fajitas',
       items: [
         {
           id: 16,
-          title: 'FAJITAS FRIDA RES-POLLO-MIXTA, CERDO O CAMARONES',
-          description: 'FAJITAS ELEGIR PROTEÍNA CON 4 TORTILLAS DE TRIGO SUAVECITAS, CREMA AGRIA, PICO DE GALLO Y UN DELICIOSO GUACAMOLE.',
+          title: 'Fajitas Frida Res-Pollo-Mixta, Cerdo o Camarones',
+          description: 'Fajitas elegir proteína con 4 tortillas de trigo suaves, crema agria, pico de gallo y un delicioso guacamole.',
           price: 690,
-          
           image: 'fajitas.jpg',
           rating: 5,
           reviews: 35
         }
-
-
       ]
     },
     {
-            id: 'Otros',
-            name: 'Otros',
-            items: [
-
-
-                {
-                    id: 17,
-                    title: 'CANASTA CATRIS MIXTA',
-                    description: 'CARRITO DE SABOR: TORTILLAS DE TRIGO REBOSANTES DE SABOR, CARNE MECHA MIXTA, LECHUGA CRUJIENTE, QUESO DERRETIDO, GUACAMOLE CREMOSO, PICO DE GALLO, SALSA ROJA. ¡UNA FIESTA EN CADA BOCADO!',
-                    price: 680,
-                    image: 'https://placehold.co/600x400/FFEB3B/ffffff?text=Canasta',
-                    rating: 5,
-                    reviews: 0
-                },
-                {
-                  id: 18,
-                  title: 'FLAUTA LA POPIS RES-POLLO O MIXTO',
-                  description: 'ESCOGE TU PROTEÍNA FAVORITA Y DISFRUTA DE 4 CRUJIENTES TORTILLAS DE MAÍZ FRITAS RELLENAS DE SABROSA CARNE, ACOMPAÑADAS DE CRUJIENTE LECHUGA, QUESO RICOTTA, CREMA AGRIA, PICO DE GALLO, SALSA ROJA Y GUACAMOLE. ¡UNA FIESTA PARA EL PALADAR!',
-                  price: 490,
-                  image: '',
-                  rating: 5,
-                  reviews: 0
-                },
-                {
-                  id: 19,
-                  title: 'CHIMICHANGA LA CHIMOLTRUFIA RES-POLLO O MIXTO',
-                  description: 'ESCOGE TU PROTEÍNA FAVORITA Y DISFRUTA DE UNA DELICIOSA TORTILLA DE TRIGO XL FRITA CARGADA DE CARNE, CREMA AGRIA, QUESO FUNDIDO, RICOTTA Y GUACAMOLE, SALSA ROJA. ¡UN FESTÍN PARA TU PALADAR!',
-                  price: 490,
-                  image: '',
-                  rating: 5,
-                  reviews: 0
-                },
-                {
-                  id: 20,
-                  title: 'TINGA DE POLLO',
-                  description: 'CARNE MECHADA, SALSA ESPECIAL DE CHILE Y CHIPOTLE, AGUACATE CREMOSO, QUESO RICOTTA, LECHUGA FRESCA Y UN TOQUE DE CREMA AGRIA, TODO APILADO EN 2 CRUJIENTES TOSTADAS DE MAÍZ.',
-                  price: 490,
-                  image: '',
-                  rating: 5,
-                  reviews: 0
-                },
-                {
-                  id: 21,
-                  title: 'TORTA LA CHINGONA',
-                  description: 'PAN RECIÉN HORNEADO, CARNE A LA PLANCHA, POLLO O CERDO, QUESO, CRUJIENTE REPOLLO, JUGOSO TOMATE, CEBOLLA, CHORIZO Y UNA DELICIOSA SALSA SECRETA DE LA CASA.',
-                  price: 680,
-                  image: 'chingona.jpg',
-                  rating: 5,
-                  reviews: 0
-                }
-            ]
+      id: 'otros',
+      name: 'Otros',
+      items: [
+        {
+          id: 17,
+          title: 'Canasta Catris Mixta',
+          description: 'Carrito de sabor: tortillas de trigo rebosantes de sabor, carne mecha mixta, lechuga crujiente, queso derretido, guacamole cremoso, pico de gallo, salsa roja. ¡Una fiesta en cada bocado!',
+          price: 680,
+          image: 'https://placehold.co/600x400/FFEB3B/ffffff?text=Canasta',
+          rating: 5,
+          reviews: 0
+        },
+        {
+          id: 18,
+          title: 'Flauta La Popis Res-Pollo o Mixto',
+          description: 'Escoge tu proteína favorita y disfruta de 4 crujientes tortillas de maíz fritas rellenas de sabrosa carne, acompañadas de crujiente lechuga, queso ricotta, crema agria, pico de gallo, salsa roja y guacamole. ¡Una fiesta para el paladar!',
+          price: 490,
+          image: '',
+          rating: 5,
+          reviews: 0
+        },
+        {
+          id: 19,
+          title: 'Chimichanga La Chimoltrufia Res-Pollo o Mixto',
+          description: 'Escoge tu proteína favorita y disfruta de una deliciosa tortilla de trigo XL frita cargada de carne, crema agria, queso fundido, ricotta y guacamole, salsa roja. ¡Un festín para tu paladar!',
+          price: 490,
+          image: '',
+          rating: 5,
+          reviews: 0
+        },
+        {
+          id: 20,
+          title: 'Tinga de Pollo',
+          description: 'Carne mechada, salsa especial de chile y chipotle, aguacate cremoso, queso ricotta, lechuga fresca y un toque de crema agria, todo apilado en 2 crujientes tostadas de maíz.',
+          price: 490,
+          image: '',
+          rating: 5,
+          reviews: 0
+        },
+        {
+          id: 21,
+          title: 'Torta La Chingona',
+          description: 'Pan recién horneado, carne a la plancha, pollo o cerdo, queso, crujiente repollo, jugoso tomate, cebolla, chorizo y una deliciosa salsa secreta de la casa.',
+          price: 680,
+          image: 'chingona.jpg',
+          rating: 5,
+          reviews: 0
+        }
+      ]
     }
   ]);
 
