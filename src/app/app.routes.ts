d798b4f9583f9022f3ed851eb4c62044b01ab0b1
dewinsonbusiness <dewinsoncruz90@gmail.com>
2026-02-17 21:46:47 -0400

    Refactor routing to use dynamic imports for menu and admin components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
-import path from 'path';
-import { MenuComponent } from './views/menu/menu.component';
 
 export const routes: Routes = [
 
     {path: '',component: HomeComponent},
-    {path: 'menu',component: MenuComponent
-
-
-    }
+ {
+    path: 'menu', loadComponent: () => import('./views/menu/menu.component').then(m => m.MenuComponent)
+ },
+ {
+    path: 'admin', loadComponent: () => import('./components/form/form/form.component').then(m => m.FormComponent)
+ },
+ {
+   path: 'form', loadComponent: () => import('./components/form/form/form.component').then(m => m.FormComponent)
+ }
 
 ];
